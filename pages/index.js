99502aa989b94851899183e550298fb56e7a7898
Kent Miguel <kentmiguel10@gmail.com>
2022-06-12 12:35:22 -0400

    Initial Files
    Started working on "Connect to Wallet" button

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head"
+import Image from "next/image"
+import styles from "../styles/Home.module.css"
+import ManualHeader from "../components/ManualHeader"
+
+export default function Home() {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Smart Contract Lottery</title>
+                <meta name="description" content="Our Smart Contract Lottery" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            {/**Create a header / connect Button / nav bar */}
+            <ManualHeader />
+            Hello!
+        </div>
+    )
+}
